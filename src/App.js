e48480c1f2bde26c15c45d850e945830beac0576
Your Name <imadmato@gmail.com>
2022-09-05 14:52:13 -0700

    croma school hero section

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Hero from './components/hero';
+import Header from './components/header';
+
+function App() {
+  return (
+   <div>
+<Hero />
+
+   </div>
+  );
+}
+
+export default App;
